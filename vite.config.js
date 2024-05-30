f236d69dc54963bdb6da6771abaafff3a0d886c8
Danil Sherbakov <dandiaborn@gmail.com>
2024-05-30 16:41:27 +0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Настройки для сборки проекта
+    // Переопределение MIME-типов для JavaScript файлов
+    rollupOptions: {
+      output: {
+        // Указываем MIME-тип application/javascript для всех .js файлов
+        // Форматирование в объекте для rollupOptions
+        format: 'es',
+        manualChunks: {},
+      },
+    },
+  }
 })
